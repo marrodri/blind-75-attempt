f09907080505ffe628baa9c8062050c02c5d10c5
marrodri <laza.mar64@gmail.com>
2023-06-19 16:01:03 -0700

    longestSubStrPalindrom: isPalindrome function added

--- mediumProblems/longestSubStrPalindrome.js
+++ mediumProblems/longestSubStrPalindrome.js
@@ -3,16 +3,40 @@
  * @return {boolean}
  */
 var isStringPalindrome = function (arr) {
+  /**
+   * first check its length, calculate if its even or odd the number of characters.
+   * //if even, stack the first half and pop with the other half.
+   * //if odd, stack the first half, then the middle character is skipped, and pop the other half.
+   *
+   * if the top character is a different character from latter half of the string, return false
+   *
+   * otherwise, when the stack is empty and the iterator reached the end of the array. return true.
+   *
+   * SMALL DETAILS
+   *  TO MEMORIZE:
+   *  -the returned values of 7/2=3.5, 8/2=4, 7%2=1, 8%2 = 0
+   *      --decimal point not showing only when it has a division without remained
+   *  -Javascript shows decimals, variables don't have a specific datatype
+   *  -its not like c++ where the variables must have a datatype, having its limitations depending the datatype.
+   */
   var stack = Array();
-  for (var i = 0; i < arr.length; i++) {
-    if (i < arr.length / 2) {
-      stack.push();
-	  //TODO:finish this function
-    } else if (i > arr.length / 2 && arr[i] == stack[i]) {
-      stack.pop();
-    }
+  var middleIndex = Math.floor(arr.length / 2); //round
+  var isOdd = arr.length % 2;
+  console.log("arr length:" + middleIndex);
+
+  for (var i = 0; i < middleIndex; i++) {
+    stack.push(arr[i]);
   }
-  return false;
+  middleIndex += isOdd;
+  for (var i = middleIndex; i < arr.length; i++) {
+    if (arr[i] !== stack[stack.length-1]) {
+      console.log("is NOT a palindrome");
+      return false;
+    }
+    stack.pop();
+  }
+  console.log("is A palindrome");
+  return true;
 };
 
 /**
@@ -37,3 +61,5 @@ var longestPalindrome = function (s) {
    */
   return palindromeSubStr;
 };
+
+isStringPalindrome("a b ada b a");
