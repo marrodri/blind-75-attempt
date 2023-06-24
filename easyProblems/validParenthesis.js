0a74932448120b8eefcfd08208f195dfc50e71d4
marrodri <laza.mar64@gmail.com>
2023-06-23 23:02:32 -0700

    validParenthesis problem resolved

---
+++ easyProblems/validParenthesis.js
@@ -0,0 +1,37 @@
+/***
+ * -Problem Description:
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ * An input string is valid if:
+
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  var stack = [];
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else if (
+      (stack[stack.length - 1] === "(" && s[i] === ")") ||
+      (stack[stack.length - 1] === "{" && s[i] === "}") ||
+      (stack[stack.length - 1] === "[" && s[i] === "]")
+    ) {
+      stack.pop();
+    }
+	else {
+		return false;
+	}
+  }
+//   this gives a below average speed time result
+  if (!stack.length) {
+    return true;
+  }
+  return false;
+};
---
+++ mediumProblems/maxWaterContainer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  /**
+   ** Based on the problem it seems that DP is going to be needed, a hash table is needed
+   * for storing the possible combinations and calculate them at the end of the problem.
+   * //This sounds trivial.
+   * 
+   * 
+   * Brute force approach.
+   * -have a variable to store the maximum area.
+   * -
+   *
+   *
+   */
+};
