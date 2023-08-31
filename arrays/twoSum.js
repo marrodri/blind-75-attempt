d08880e1df6d94cd374feda864d86ac9f4771407
marrodri <laza.mar64@gmail.com>
2023-08-30 20:14:40 -0700

    current coding challenges file restructure

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -1,4 +1,13 @@
 /**
+ * TwoSum Problem:
+ *  Given an array of integers nums and an integer target,
+ *   return indices of the two numbers such that they add up to target.
+ *  You may assume that each input would have exactly one solution,
+ *  and you may not use the same element twice.
+ *  You can return the answer in any order.
+ */
+
+/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -7,16 +16,8 @@ var twoSum = function (nums, target) {
   // the function returns an array
   // of indices, indicating the indexed
   // numbers that form the value,
-  // when summing  2 numbers
-
-  /**
-   * it looks like it needs a double
-   * nested for loop. where each loop
-   * is going. when a sum is encountered,
-   * stop the iteration and return both
-   * values as an array.
-   */
-
+  // when summing 2 numbers
+  
   for (var i = 0; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -27,5 +28,5 @@ var twoSum = function (nums, target) {
   return [];
 };
 
-var nums = twoSum([1,2,1], 3);
+var nums = twoSum([1, 2, 1], 3);
 console.log(nums);
